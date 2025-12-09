1ade85a3850433f6c6d36f4bc1e8e3e1ca74a105
badalaryal11 <badal.aryal@gmail.com>
2025-12-09 11:45:13 +0545

    Enhance admin dashboard configuration

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,27 @@
 from django.contrib import admin
 from .models import Project, BlogPost, Subscriber, PageVisit
 
-admin.site.register(Project)
-admin.site.register(BlogPost)
-admin.site.register(Subscriber)
+admin.site.site_header = "Portfolio Administration"
+admin.site.site_title = "Portfolio Admin Portal"
+admin.site.index_title = "Welcome to the Portfolio Admin"
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'tech_stack', 'created_at')
+    search_fields = ('title', 'description', 'tech_stack')
+    list_filter = ('created_at',)
+
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date_posted', 'canonical_url')
+    search_fields = ('title', 'content')
+    list_filter = ('date_posted',)
+
+@admin.register(Subscriber)
+class SubscriberAdmin(admin.ModelAdmin):
+    list_display = ('email', 'date_subscribed')
+    search_fields = ('email',)
+    list_filter = ('date_subscribed',)
 
 @admin.register(PageVisit)
 class PageVisitAdmin(admin.ModelAdmin):
