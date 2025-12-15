ff8cc42be148db1e2bab719dbcfa97e5faefa0d6
badalaryal11 <badal.aryal@gmail.com>
2025-12-15 13:39:58 +0545

    Revert "Configure Cloudflare IPs and AllowCIDRMiddleware"
    This reverts commit b547522be34ae3ec36e3e3b60f53b2e164bb2ae2.

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -29,32 +29,6 @@ DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = ['badal-aryal.com.np', 'www.badal-aryal.com.np', 'badalaryal11.pythonanywhere.com', '127.0.0.1', 'localhost', '.vercel.app']
 
-# Cloudflare IP ranges (IPv4 and IPv6)
-ALLOWED_CIDR_NETS = [
-    '173.245.48.0/20',
-    '103.21.244.0/22',
-    '103.22.200.0/22',
-    '103.31.4.0/22',
-    '141.101.64.0/18',
-    '108.162.192.0/18',
-    '190.93.240.0/20',
-    '188.114.96.0/20',
-    '197.234.240.0/22',
-    '198.41.128.0/17',
-    '162.158.0.0/15',
-    '104.16.0.0/13',
-    '104.24.0.0/14',
-    '172.64.0.0/13',
-    '131.0.72.0/22',
-    '2400:cb00::/32',
-    '2606:4700::/32',
-    '2803:f800::/32',
-    '2405:b500::/32',
-    '2405:8100::/32',
-    '2a06:98c0::/29',
-    '2c0f:f248::/32',
-]
-
 CSRF_TRUSTED_ORIGINS = ['https://badal-aryal.com.np', 'https://www.badal-aryal.com.np']
 
 if not DEBUG:
@@ -80,7 +54,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'allow_cidr.middleware.AllowCIDRMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
